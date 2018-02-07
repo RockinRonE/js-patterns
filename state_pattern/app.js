32bb70be75d1ad2afabc26d838552904d62759ae
Ronny Ewanek <ronnye@criticalmass.com>
2018-02-07 15:36:41 -0600

    Add finished State Patter

--- state_pattern/app.js
+++ state_pattern/app.js
@@ -2,7 +2,7 @@ const PageState = function() {
 	let currentState = new homeState(this)
 
 	this.init = function() {
-		this.change(new homeState())
+		this.change(new aboutState)
 	}
 
 	this.change = function(state) {
@@ -27,8 +27,74 @@ const homeState = function(page) {
 }
 
 const aboutState = function(page) {
-	document.querySelector('.heading').textContent = 'About Us'
+	document.querySelector('#heading').textContent = 'About Us'
 	document.querySelector('#content').innerHTML = `
 		<p>This is the About Page</p>
 	`
 }
+
+const contactState = function(page) {
+	document.querySelector('#heading').textContent = 'Contact Us'
+	document.querySelector('#content').innerHTML = `
+		<form>
+			<div class="form-group">
+				<label>Name</label>
+				<input type='text' class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input type='email' class="form-control">
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	`
+}
+
+
+// Instantiate pageState
+const page = new PageState()
+
+// init first state
+page.init()
+
+// UI vars
+const home = document.getElementById('home'),
+	about = document.getElementById('about'),
+	contact = document.getElementById('contact')
+
+// Home
+home.addEventListener('click', e => {
+	page.change(new homeState)
+
+	e.preventDefault()
+})
+
+// About
+about.addEventListener('click', e => {
+	page.change(new aboutState)
+
+	e.preventDefault()
+})
+
+// Contact
+contact.addEventListener('click', e => {
+	page.change(new contactState)
+
+	e.preventDefault()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
